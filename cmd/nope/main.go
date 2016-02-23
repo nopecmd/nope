cd2760f1b0c4e2a7601985607324d415f03dfb49
Narendran Srinivasan <narendran@remind101.com>
2016-02-22 21:19:06 -0800

    Outputs previous bash command

--- cmd/nope/main.go
+++ cmd/nope/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
-	_ "github.com/nopecmd/nope/match"
+	"bufio"
+	"fmt"
+	"os"
 
 	_ "github.com/nopecmd/nope/commands"
+	_ "github.com/nopecmd/nope/match"
 )
 
+// Don't know how to make this a constant
+var HISTFILE = os.Getenv("HOME") + "/.bash_history"
+
+func getLastLine(fname string) string {
+	file, err := os.Open(fname)
+
+	if err != nil {
+		fmt.Println("Can't open bash history file!")
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var x = ""
+	for scanner.Scan() {
+		x = scanner.Text()
+	}
+	return x
+}
+
 func main() {
+	fmt.Println(getLastLine(HISTFILE))
 }
