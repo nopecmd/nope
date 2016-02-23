a35f7819d6fac4066d4f3bb810aba66542d5141c
Rossem <rrsbrg@gmail.com>
2016-02-23 01:02:14 -0800

    add logic to match rule

--- match/match.go
+++ match/match.go
@@ -1,13 +1,25 @@
 package match
 
+import "errors"
+
 type rule struct {
 	IsMatch func(string) bool
 	GetUndo func(string) string
 }
 
-var Cmds []rule
+var cmdRules []rule
 
 func AddCommand(isMatch func(string) bool, getUndo func(string) string) {
 	var cmdRule = rule{IsMatch: isMatch, GetUndo: getUndo}
-	Cmds = append(Cmds, cmdRule)
+	cmdRules = append(cmdRules, cmdRule)
+}
+
+func GetUndoCommand(prevCmd string) (string, error) {
+	for _, rule := range cmdRules {
+		if rule.IsMatch(prevCmd) {
+			return rule.GetUndo(prevCmd), nil
+		}
+	}
+
+	return "", errors.New("Unable to match command")
 }
