c3c1cb75dbdf48e939a3e85bdd80fd19367d4348
Rossem <rrsbrg@gmail.com>
2016-02-22 23:26:15 -0800

    removed useless logging in main

--- cmd/nope/main.go
+++ cmd/nope/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/nopecmd/nope/match"
+	_ "github.com/nopecmd/nope/match"
 
 	_ "github.com/nopecmd/nope/commands"
 )
 
 func main() {
-	fmt.Println(match.Cmds[0].GetUndo("cd weqwe"))
-	fmt.Println(match.Cmds[1].GetUndo("cd weqwe"))
 }
