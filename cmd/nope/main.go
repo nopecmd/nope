74971e9b2ee8755619f60d1aee83f555009c8820
Rostislav Semenov <rrsbrg@gmail.com>
2016-02-27 22:45:32 -0800

    remove useless trash from main

--- cmd/nope/main.go
+++ cmd/nope/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/nopecmd/nope/match"
-	"github.com/nopecmd/nope/parse"
 	_ "github.com/nopecmd/nope/rules"
 	"github.com/nopecmd/nope/shells"
 )
@@ -11,24 +9,4 @@ import (
 func main() {
 	// Print the previous command depending on 'CurrentShell'
 	fmt.Println(shells.CurrentShell.GetLastCmd())
-
-	// test cd
-	var cdRaw = "cd .."
-	var cdCmd = parse.ParseCommand(cdRaw)
-
-	undoCd, err := match.GetUndoCommand(cdCmd)
-	if err != nil {
-		fmt.Println("donezo")
-	}
-	fmt.Println(undoCd)
-
-	// test git add -A with extra spaces
-	var gitAddAllRaw = "git    add    -A"
-	var gitAddAllCmd = parse.ParseCommand(gitAddAllRaw)
-
-	undoGitAddAll, err := match.GetUndoCommand(gitAddAllCmd)
-	if err != nil {
-		fmt.Println("donezo")
-	}
-	fmt.Println(undoGitAddAll)
 }
